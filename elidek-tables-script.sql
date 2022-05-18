6ee80c6475546c2e333f0abb6bd63108090b0926
George Sotiropoulos <76242811+GeorgeSot71@users.noreply.github.com>
2022-05-18 16:00:25 +0300

    Update elidek-tables-script.sql

--- elidek-tables-script.sql
+++ elidek-tables-script.sql
@@ -1,32 +1,32 @@
 DROP DATABASE elidek;
-
 CREATE DATABASE elidek;
-
 USE elidek;
 
-CREATE TABLE project (
-    project_id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    summary VARCHAR(100000) NOT NULL,
-    funding DECIMAL(13,2) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    abbreviation VARCHAR(30) NOT NULL,
-    duration VARCHAR(50),
-    program_id INT NOT NULL,
-    executive_id INT NOT NULL,
-    program_id INT NOT NULL,
-    scientific_inspector_id INT NOT NULL,
-    PRIMARY KEY (project_id),
-    FOREIGN KEY (abbreviation)
-        REFERENCES organization(abbreviation),
-    FOREIGN KEY (executive_id)
-        REFERENCES executive(executive_id),
-    FOREIGN KEY (program_id)
-        REFERENCES program(program_id),
-    FOREIGN KEY (scientific_inspector_id)
-        REFERENCES researcher(researcher_id)
-);
+CREATE TABLE program(
+    program_id INT NOT NULL AUTO_INCREMENT,
+    program_name VARCHAR(30) NOT NULL,
+    adress VARCHAR(30) NOT NULL,
+    PRIMARY KEY (program_id)
+)ENGINE=INNODB;
+
+CREATE TABLE executive (
+    executive_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    surname VARCHAR(30) NOT NULL,
+    birthday DATE NOT NULL,
+    sex VARCHAR(10) NOT NULL,
+    PRIMARY KEY (executive_id)
+)ENGINE=INNODB;
+
+CREATE TABLE researcher (
+    researcher_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    age INT,
+    surname VARCHAR(30) NOT NULL,
+    birthday DATE NOT NULL,
+    sex VARCHAR(10) NOT NULL,
+    PRIMARY KEY (researcher_id)
+)ENGINE=INNODB;
 
 CREATE TABLE organization (
     /*
@@ -39,7 +39,7 @@ CREATE TABLE organization (
     road VARCHAR(30) NOT NULL,
     town VARCHAR(30) NOT NULL,
     PRIMARY KEY (abbreviation)
-);
+)ENGINE=INNODB;
 
 CREATE TABLE phone (
     abbreviation VARCHAR(30) NOT NULL,
@@ -47,17 +47,32 @@ CREATE TABLE phone (
     FOREIGN KEY (abbreviation)
         REFERENCES organization(abbreviation)
     /* https://dev.mysql.com/doc/refman/8.0/en/example-foreign-keys.html */
-);
+)ENGINE=INNODB;
 
-CREATE TABLE researcher (
-    researcher_id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    age INT,
-    surname VARCHAR(30) NOT NULL,
-    birthday DATE NOT NULL,
-    sex VARCHAR(10) NOT NULL,
-    PRIMARY KEY (researcher_id)
-);
+CREATE TABLE university (
+    abbreviation VARCHAR(30) NOT NULL,
+    budget DECIMAL(13,2) NOT NULL,
+    PRIMARY KEY (abbreviation),
+    FOREIGN KEY (abbreviation)
+        REFERENCES organization(abbreviation)
+)ENGINE=INNODB;
+
+CREATE TABLE scientific_center (
+    abbreviation VARCHAR(30) NOT NULL,
+    budget_ministry DECIMAL(13,2) NOT NULL,
+    budget_private DECIMAL(13,2) NOT NULL,
+    PRIMARY KEY (abbreviation),
+    FOREIGN KEY (abbreviation)
+        REFERENCES organization(abbreviation)
+)ENGINE=INNODB;
+
+CREATE TABLE company (
+    abbreviation VARCHAR(30) NOT NULL,
+    private_funding DECIMAL(13,2) NOT NULL,
+    PRIMARY KEY (abbreviation),
+    FOREIGN KEY (abbreviation)
+        REFERENCES organization(abbreviation)
+)ENGINE=INNODB;
 
 CREATE TABLE employee_relation (
     researcher_id INT NOT NULL,
@@ -67,48 +82,41 @@ CREATE TABLE employee_relation (
         REFERENCES researcher(researcher_id),
     FOREIGN KEY (abbreviation)
         REFERENCES organization(abbreviation)
-);
+)ENGINE=INNODB;
+
+CREATE TABLE project (
+    project_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    summary TEXT NOT NULL,
+    funding DECIMAL(13,2) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    abbreviation VARCHAR(30) NOT NULL,
+    duration VARCHAR(50),
+    funding_program_id INT NOT NULL,
+    executive_id INT NOT NULL,
+    program_id INT NOT NULL,
+    scientific_inspector_id INT NOT NULL,
+    PRIMARY KEY (project_id),
+    FOREIGN KEY (abbreviation)
+        REFERENCES organization(abbreviation),
+    FOREIGN KEY (executive_id)
+        REFERENCES executive(executive_id),
+    FOREIGN KEY (funding_program_id)
+        REFERENCES program(program_id),
+    FOREIGN KEY (scientific_inspector_id)
+        REFERENCES researcher(researcher_id)
+)ENGINE=INNODB;
 
 CREATE TABLE deliverable (
     title VARCHAR(30) NOT NULL,
-    summary VARCHAR(100000) NOT NULL,
+    summary TEXT NOT NULL,
     delivery_date DATE NOT NULL,
     project_id INT NOT NULL,
     PRIMARY KEY (title),
     FOREIGN KEY (project_id)
         REFERENCES project(project_id)
-);
-
-CREATE TABLE program(
-    program_id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    adress VARCHAR(30) NOT NULL
-);
-
-CREATE TABLE university (
-    abbreviation VARCHAR(30) NOT NULL,
-    budget DECIMAL(13,2) NOT NULL,
-    PRIMARY KEY (abbreviation),
-    FOREIGN KEY (abbreviation)
-        REFERENCES organization(abbreviation)
-);
-
-CREATE TABLE scientific_center (
-    abbreviation VARCHAR(30) NOT NULL,
-    budget_ministry DECIMAL(13,2) NOT NULL,
-    budget_private DECIMAL(13,2) NOT NULL,
-    PRIMARY KEY (abbreviation),
-    FOREIGN KEY (abbreviation)
-        REFERENCES organization(abbreviation)
-);
-
-CREATE TABLE company (
-    abbreviation VARCHAR(30) NOT NULL,
-    private_funding DECIMAL(13,2) NOT NULL,
-    PRIMARY KEY (abbreviation),
-    FOREIGN KEY (abbreviation)
-        REFERENCES organization(abbreviation)
-);
+)ENGINE=INNODB;
 
 CREATE TABLE works_on_project(
     project_id INT NOT NULL,
@@ -117,7 +125,7 @@ CREATE TABLE works_on_project(
 		REFERENCES project(project_id),
     FOREIGN KEY (researcher_id)
 		REFERENCES researcher(researcher_id)
-);
+)ENGINE=INNODB;
 
 CREATE TABLE evaluate_project(
     project_id INT NOT NULL,
@@ -128,20 +136,11 @@ CREATE TABLE evaluate_project(
         REFERENCES project(project_id),
     FOREIGN KEY (researcher_id)
         REFERENCES researcher(researcher_id)
-);
-
-CREATE TABLE executive (
-    executive_id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    surname VARCHAR(30) NOT NULL,
-    birthday DATE NOT NULL,
-    sex VARCHAR(10) NOT NULL,
-    PRIMARY KEY (executive_id)
-);
+)ENGINE=INNODB;
 
 CREATE TABLE scientific_field (
     project_id INT NOT NULL,
     scientific_field_name VARCHAR(50) NOT NULL,
     FOREIGN KEY (project_id)
         REFERENCES project(project_id)
-);
+)ENGINE=INNODB;

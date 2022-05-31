7690c2dd47e368e387772fb99cae93146d82a050
miketsili <97021645+miketsili@users.noreply.github.com>
2022-05-31 14:11:58 +0300

    Update insertions.sql

--- insertions.sql
+++ insertions.sql
@@ -868,13 +868,13 @@ CREATE TABLE `scientific_center` (
 INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('StM', '344118.13', '390846.01');
 INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('RuInc', '882819.05', '650854.50');
 INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('RunBa', '444897.49', '105802.96');
-INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('RLLC', '313455.12', '57152.37');
+INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('RLLC', '313455.12', '570152.37');
 INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('RyanG', '391593.04', '289355.33');
 INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('GK', '117597.30', '244442.10');
 INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('SteKo', '377559.72', '276355.49');
-INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('LeBa', '362546.43', '20530.20');
-INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('St.M', '133377.78', '37897.54');
-INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('Mupu', '299595.43', '23106.45');
+INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('LeBa', '362546.43', '205030.20');
+INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('St.M', '133377.78', '370897.54');
+INSERT INTO `scientific_center` (`abbreviation`, `budget_ministry`, `budget_private`) VALUES ('Mupu', '299595.43', '231006.45');
 
 
 #

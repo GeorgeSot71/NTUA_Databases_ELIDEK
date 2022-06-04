0a4f7090fc5c8e793d6dce1e5c763108b1b01037
nospa42 <63493812+nospa42@users.noreply.github.com>
2022-06-04 20:06:33 +0300

    Add files via upload

--- sql files/elidek-tables-script.sql
+++ sql files/elidek-tables-script.sql
@@ -192,3 +192,18 @@ CREATE TRIGGER same_researcher_evaluator BEFORE INSERT ON works_on_project
 		END;$$
 
 DELIMITER ;
+
+CREATE VIEW projects_per_researcher
+AS
+SELECT r.name,r.surname, p.title
+FROM researcher r, project p
+WHERE r.researcher_id IN (SELECT researcher_id FROM works_on_project WHERE project_id = p.project_id)
+ORDER BY r.researcher_id;
+
+CREATE VIEW organization_info
+AS
+SELECT org.abbreviation ,org.name ,org.post_code, org.road, org.town, ph.phone_number
+FROM organization org
+LEFT JOIN
+phone ph
+ON ph.abbreviation = org.abbreviation;
